a25e47f896df8b8f6ad38a3314c4fed1a1d7fa37
Damon <dong765@qq.com>
2025-04-15 11:02:28 +0800

    language

--- Pods/DDKitSwift_NetCheck.swift
+++ Pods/DDKitSwift_NetCheck.swift
@@ -15,6 +15,21 @@ func UIImageHDBoundle(named: String?) -> UIImage? {
     return UIImage(named: name, in: bundle, compatibleWith: nil)
 }
 
+extension String{
+    var ZXLocaleString: String {
+        //优先使用主项目翻译
+        let mainValue = NSLocalizedString(self, comment: "")
+        if mainValue != self {
+            return mainValue
+        }
+        //使用自己的bundle
+        if let bundlePath = Bundle(for: DDKitSwift_NetCheck.self).path(forResource: "DDKitSwift_NetCheck", ofType: "bundle"), let bundle = Bundle(path: bundlePath) {
+            return NSLocalizedString(self, tableName: nil, bundle: bundle, value: self, comment: "")
+        }
+        return self
+    }
+}
+
 //ZXKitPlugin
 open class DDKitSwift_NetCheck: DDKitSwiftPluginProtocol {
     public var isRunning: Bool = false
@@ -33,11 +48,11 @@ open class DDKitSwift_NetCheck: DDKitSwiftPluginProtocol {
     }
 
     public var pluginTitle: String {
-        return NSLocalizedString("Net check", comment: "")
+        return "Network Diagnosis".ZXLocaleString
     }
 
     public var pluginType: DDKitSwiftPluginType {
-        return .ui
+        return .other
     }
 
     public func start() {
